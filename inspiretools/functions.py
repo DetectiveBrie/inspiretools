8be468c7f2bee59016d51a6817f34f40a3e246f7
Nicolas Chagnet <chagnet@lorentz.leidenuniv.nl>
2023-08-17 13:53:33 +0200

    adapted the blg function to find and append the associated .bib file

--- inspiretools/functions.py
+++ inspiretools/functions.py
@@ -46,7 +46,7 @@ def blg2texkey(filename):
     keys = []
     if not os.path.exists(filename):
         LOGGER.error("File %s not found.", filename)
-        return keys
+        return (keys,None)
     with open(filename, "r") as f:
         lines = f.readlines()
     # regexp to match 'Warning--I didn\'t find a database entry for "..."' (bibtex)
@@ -66,7 +66,20 @@ def blg2texkey(filename):
     keys = list(set(keys))
     # remove blacklisted keys
     keys = [x for x in keys if x not in BLACKLISTED_KEYS]
-    return keys
+    
+    # regexp to match 'Database file #1: xxx.bib
+    pattern_filename = re.compile(
+            r"^Database file #1: (?P<file>.*)$"
+    )
+    # Extract name of bib file
+    bibfiles = [
+        re.search(pattern_filename, c).group("file")
+        for c in lines
+        if re.match(pattern_filename, c)
+    ]
+    bibfile = bibfiles[0] if len(bibfiles) > 0 else None
+
+    return (keys,bibfile)
 
 
 def onerr(err, texkey):
@@ -86,6 +99,7 @@ def texkey2bib(texkeys):
     tot = len(texkeys)
     err = []
     notfound = []
+    result = []
     for i, texkey in enumerate(texkeys):
         try:
             LOGGER.info("Looking up reference %s of %s", str(i + 1), str(tot))
@@ -95,7 +109,7 @@ def texkey2bib(texkeys):
             res.raise_for_status()
             bib = res.text
             if bib != "":
-                print(bib)
+                result.append(bib)
                 LOGGER.info("Success.")
             else:
                 notfound.append(texkey)
@@ -118,7 +132,7 @@ def texkey2bib(texkeys):
             "The following references could not be found on INSPIRE:\n%s",
             "\n".join(notfound),
         )
-
+    return result
 
 def aux2bib():
     """Generate bibliography from .aux file."""
@@ -133,6 +147,21 @@ def blg2bib():
     """Generate bibliography from .blg file."""
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="BibTeX .blg file", type=str)
+    parser.add_argument("--addtobib", help="Append bib entries to .bib files defined in the .blg file", action='store_true', default=False)
     args = parser.parse_args()
-    texkeys = blg2texkey(args.file)
-    texkey2bib(texkeys)
+    
+    texkeys, bibfile = blg2texkey(args.file) #Obtain keys and .bib file from .blg
+    bibs = texkey2bib(texkeys) # convert valid tex keys to inspirehep entries
+    bibstr = '\n'.join(bibs) # concatenate bib pieces into one
+    
+    # check existence of .bib file
+    if (bibfile is None) or (not os.path.exists(bibfile)):
+        LOGGER.error("File %s not found.", bibfile)
+        args.addtobib = False
+
+    # Either print or add bib entries to .bib file
+    if (not args.addtobib) or len(bibs) == 0:
+        print(bibstr)
+    else:
+        with open(bibfile, 'a') as f:
+            f.write('\n' + bibstr)
